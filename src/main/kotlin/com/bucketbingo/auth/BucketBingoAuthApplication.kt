875e30a2d43b3d6c7320c0a196ff95c177d6fa42
injuk <ingnoh1001@gmail.com>
2024-01-30 19:30:18 +0900

    chore: application.properties 삭제

--- src/main/kotlin/com/bucketbingo/auth/BucketBingoAuthApplication.kt
+++ src/main/kotlin/com/bucketbingo/auth/BucketBingoAuthApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class AuthApplication
+class BucketBingoAuthApplication
 
 fun main(args: Array<String>) {
-	runApplication<AuthApplication>(*args)
+	runApplication<BucketBingoAuthApplication>(*args)
 }
